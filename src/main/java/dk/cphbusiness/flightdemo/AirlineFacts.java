15c20a9c44b142d84f2c91b10967bd095e6962bf
knogleknuser <145555271+knogleknuser@users.noreply.github.com>
2024-08-21 11:58:50 +0200

    AirlineFacts, ehhhhh, try again later

---
+++ src/main/java/dk/cphbusiness/flightdemo/AirlineFacts.java
@@ -0,0 +1,129 @@
+package dk.cphbusiness.flightdemo;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+public class AirlineFacts
+{
+    
+    private List< DTOs.FlightInfo > flightInfoList;
+    
+    private final Set< String > airlinesSet = new TreeSet<>();
+    
+    public AirlineFacts( List< DTOs.FlightInfo > flightInfoList )
+    {
+        this.flightInfoList = flightInfoList;
+        
+        this.calcAirlines( flightInfoList );
+    }
+    
+    private void calcAirlines( List< DTOs.FlightInfo > flightInfoList )
+    {
+        if ( flightInfoList == null ) {
+            System.err.println( "Flight info was null" );
+            return;
+        }
+        
+        this.airlinesSet.clear();
+        
+        this.airlinesSet.addAll(
+                flightInfoList.stream()
+                        .map( flightInfo -> flightInfo.getAirline() )
+                        .toList()
+                               );
+        
+    }
+    
+    public Set< String > getAirlinesSet()
+    {
+        return this.airlinesSet;
+    }
+    
+    private List< DTOs.FlightInfo > filterOutRelevantAirlines( String... airline )
+    {
+        Set< String > inputAirlineSet = Arrays.stream( airline ).collect( Collectors.toSet() );
+        
+        List< DTOs.FlightInfo > relevantAirlines = this.flightInfoList.stream()
+                .filter( flightInfo -> inputAirlineSet.contains( flightInfo.getAirline() ) )
+                .collect( Collectors.toList() );
+        
+        return relevantAirlines;
+    }
+    
+//    public Map< String, List< Duration > > getFlightDurationsByAirline( String... airline )
+//    {
+//        List< DTOs.FlightInfo > relevantAirlines = this.filterOutRelevantAirlines( airline );
+//
+//        Map< String, List< Duration > > flightDurationsByAirline = relevantAirlines.stream()
+//                .collect( Collectors.groupingBy( flightInfo -> flightInfo.getAirline(),
+//                                                 aver
+//                                               )
+//                        )
+////                .map( flightInfos -> flightInfos.stream().map( flightInfo -> flightInfo.getDuration() ).toList() )
+//                .
+//                ;
+//
+//        return flightDurationsByAirline;
+//    }
+    
+    public Map< String, Duration > calcAverageFlightDuration( String... airline )
+    {
+//        Set< String > inputAirlineSet = Arrays.stream( airline ).collect( Collectors.toSet() );
+//
+//        Map< String, List< Duration > > flightMapDurationList = new HashMap<>();
+//
+//        Map< String, Duration > averageFlightDuration = new TreeMap<>();
+//
+//        Map< String, Duration > averageFlightDurationRes = this.flightInfoByAirlineList.entrySet()
+//                .stream()
+//                .filter( stringListEntry -> inputAirlineSet.contains( stringListEntry.getKey() ) )
+//                .map( stringListEntry -> {
+//
+//                          flightMapDurationList.put(
+//                                  stringListEntry.getKey(),
+//                                  stringListEntry.getValue().stream().map( flightInfo -> flightInfo.getDuration() ).toList()
+//                                                   );
+//
+//
+//                          return flightMapDurationList;
+//                      }
+//                    )
+//                .map( flightMapDurationListInput -> {
+//
+//                    flightMapDurationListInput.entrySet()
+//                            .stream()
+//                            .map(
+//                                    flightEntryDurationListInput -> {
+//                                        averageFlightDuration.put(
+//                                                flightEntryDurationListInput.getKey(),
+//                                                Duration.of(
+//                                                        ( long ) flightEntryDurationListInput.getValue()
+//                                                                .stream()
+//                                                                .mapToLong( flightDuration -> flightDuration.toSeconds() )
+//                                                                .average()
+//                                                                .getAsDouble(),
+//                                                        ChronoUnit.SECONDS )
+//                                                                 );
+//
+//                                        return averageFlightDuration;
+//                                    }
+//                                );
+//
+//                    return averageFlightDuration;
+//
+//                } )
+//                .collect( Collectors.toMap() );
+//
+//        return averageFlightDurationRes;
+    }
+    
+    public Map< String, Duration > calcTotalFlightDuration( String... airline )
+    {
+        
+        return null;
+    }
+    
+}
---
+++ src/test/java/dk/cphbusiness/flightdemo/AirlineInfoTest.java
@@ -0,0 +1,89 @@
+package dk.cphbusiness.flightdemo;
+
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AirlineInfoTest
+{
+    
+    private static FlightReader reader = new FlightReader();
+    
+    private static List< DTOs.FlightDTO > flightList;
+    private static List< DTOs.FlightInfo > flightInfoList;
+    
+    private static AirlineFacts airlineFacts;
+    
+    @BeforeAll
+    static void beforeAll()
+    {
+        try {
+            flightList = reader.getFlightsFromFile( "flights.json" );
+            
+        } catch ( IOException e ) {
+            throw new RuntimeException( e );
+        }
+        
+        flightInfoList = reader.getFlightInfoDetails( flightList );
+        
+        airlineFacts = new AirlineFacts( flightInfoList );
+    }
+    
+    @BeforeEach
+    void setUp()
+    {
+    }
+    
+    @AfterEach
+    void tearDown()
+    {
+    }
+    
+    @Test
+    @DisplayName( "Airline Set" )
+    void airlineSet()
+    {
+        System.out.println();
+        System.out.println( "-----AIRLINE SET-----" );
+        
+        Set< String > airlines = airlineFacts.getAirlinesSet();
+        
+        System.out.println( airlines );
+    }
+    
+    @Test
+    @DisplayName( "Average Airline Flight Time" )
+    void averageFlightTime()
+    {
+        System.out.println();
+        System.out.println( "-----AVERAGE FLIGHT TIME-----" );
+        
+        Set< String > airlines = airlineFacts.getAirlinesSet();
+        
+        Map<String, Duration > averageFlightTime = airlineFacts.calcAverageFlightDuration( airlines.toArray( new String[ 0 ] ) );
+        
+        averageFlightTime.forEach( (k, v) -> {
+            System.out.println(k + " " + v.toSeconds() + " s");
+        });
+        
+    }
+    
+    @Test
+    @DisplayName( "Total Airline Flight Time" )
+    void totalFlightTime()
+    {
+    }
+    
+    
+    
+    
+    
+    
+    
+}
